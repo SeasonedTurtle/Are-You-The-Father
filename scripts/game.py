209710d1ac35c3e49d0e4a4ed6ac0a13bb4394ad
jdc12398 <102702019+jdc12398@users.noreply.github.com>
2022-04-26 19:39:16 -0400

    Update game.py

--- scripts/game.py
+++ scripts/game.py
@@ -196,18 +196,38 @@ class Game(consts.Constants):
 
     def scene1(self):
         self.talking("What were you doing on the night you met?", 0, 750, self.purple, 200, 200, 300, self.mauryV1)
+        self.talking("I was at this party passing out my mixtape, when this girl Monique started talking about how my music is fire.", 200, 750, self.purple, 125, 500, 1100, self.tyroneV1)
+        self.talking("We had a quick conversation, and started messing with each other.", 200, 750, self.purple, 125, 500, 650, self.tyroneV1)
+        self.talking("I was at this party; doing my daily spiritual routine. As I was taking my prescribed amount of narcotics, Monique came up to me to ask what’s up.", 200, 750, self.purple, 125, 500, 1450, self.tyroneV2)
+        self.talking("We had a quick conversation, and formed a mutual interest.", 200, 750, self.purple, 125, 500, 580, self.tyroneV2)
 
     def scene2(self):
         self.talking("How do you feel about Kevin?", 0, 750, self.purple, 150, 200, 300, self.mauryV1)
+        self.talking("Pull up to the block right now he ain’t want no smoke.", 200, 750, self.purple, 125, 500, 540, self.tyroneV1)
+        self.talking("I let my money talk", 200, 750, self.purple, 125, 500, 190, self.tyroneV1)
+        self.talking("I love that child, and even if he’s not my son or father, I’ll support him.", 200, 750, self.purple, 125, 500, 750, self.tyroneV2)
+        self.talking("He’s not mine, I gave my soul to Jah, not to the kid or that darn hippo!", 200, 750, self.purple, 125, 500, 720, self.tyroneV2)
     
     def scene3(self):
         self.talking("How did your relationship with Monique worsen?", 0, 750, self.purple, 200, 200, 300, self.mauryV1)
+        self.talking("I released some new music, and Monique thought it was trash.", 200, 750, self.purple, 125, 500, 600, self.tyroneV1)
+        self.talking("She was listening to another rapper who dissed me.", 200, 750, self.purple, 125, 500, 500, self.tyroneV1)
+        self.talking("Whenever we would go out on dates, the spiritual vibes of the activity were questionable.", 200, 750, self.purple, 125, 500, 890, self.tyroneV2)
+        self.talking("She got mad that I was a chainsmoker.", 200, 750, self.purple, 125, 500, 370, self.tyroneV2)
 
     def scene4(self):
         self.talking("So, why did you seperate?", 0, 750, self.purple, 150, 200, 300, self.mauryV1)
+        self.talking("I heard rumors that Monique was cheating on me with my twin brother.", 200, 750, self.purple, 125, 500, 680, self.tyroneV1)
+        self.talking("She didn’t like how I was kickin it with my other concubines.", 200, 750, self.purple, 125, 500, 610, self.tyroneV1)
+        self.talking("I heard rumors that Monique was cheating on me with my twin brother.", 200, 750, self.purple, 125, 500, 680, self.tyroneV2)
+        self.talking("We had a mutual determination that our futures were not aligned in the eyes of Jah. And that’s on Jah.", 200, 750, self.purple, 125, 500, 1020, self.tyroneV2)
 
     def scene5(self):
         self.talking("How do you feel about Monique now?", 0, 750, self.purple, 150, 200, 300, self.mauryV1)
+        self.talking("Four out of ten.", 200, 750, self.purple, 125, 500, 160, self.tyroneV1)
+        self.talking("She went from Nicki Minaj to Lizzo.", 200, 750, self.purple, 125, 500, 350, self.tyroneV1)
+        self.talking("Monique is another woman who needs to be enlightened.", 200, 750, self.purple, 125, 500, 530, self.tyroneV2)
+        self.talking("Monique is a liar and a sinner in the eyes of Jah. Her lust knows no bounds.", 200, 750, self.purple, 125, 500, 760, self.tyroneV2)
 
     def outro(self):
         pass
